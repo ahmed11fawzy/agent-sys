5c7f7f20a0e15a3d45122219b1245c3d58afcb58
unknown <af6394158@gmail.com>
2025-12-30 23:27:17 +0200

    prepare headers before req

--- src/features/core-api-slice.ts
+++ src/features/core-api-slice.ts
@@ -1,5 +1,6 @@
 
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import type { AuthorizedUser } from "@/types/authorized-user-type";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const Base_URL = import.meta.env.VITE_BASE_URL;
  console.log(Base_URL)
@@ -7,11 +8,18 @@ const coreApiSlice = createApi({
     reducerPath: "coreApi",
     baseQuery: fetchBaseQuery({
         baseUrl: Base_URL,
-        
+        prepareHeaders: (headers) => {
+            const user = JSON.parse(localStorage.getItem("user") as string) as AuthorizedUser;
+            if (user) {
+                headers.set("Authorization", `Bearer ${user.token}`);
+                headers.set("Accept", "application/json");
+            }
+            return headers;
+        },
         
     }),
     endpoints: () => ({}),
-
+    tagTypes: ['Agents'],
 
 });
 
--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import settingsReducer from './features/setting-slice';
+import authReducer from './features/auth-slice';
 import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import coreApiSlice from './features/core-api-slice';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
   reducer: {
     settings: settingsReducer,
+    auth: authReducer,
     coreApi: coreApiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(coreApiSlice.middleware),
 });
 
+setupListeners(store.dispatch);
 
 // Infer types
 export type RootState = ReturnType<typeof store.getState>;
